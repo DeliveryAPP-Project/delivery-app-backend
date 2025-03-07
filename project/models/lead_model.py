46bc8dc3655f78e822b794182130381522c0599c
bentoluizv <bentoluizv@gmail.com>
2025-03-07 11:43:21 -0300

    fix: remove primary key from email

--- project/models/lead_model.py
+++ project/models/lead_model.py
@@ -8,4 +8,4 @@ from ..ext.database import db
 class Lead(db.Model):
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     created_at: datetime = db.Column(db.DateTime, default=func.now())
-    email: str = db.Column(db.String(120), primary_key=True, nullable=False)
+    email: str = db.Column(db.String(120), unique=True, nullable=False, index=True)
