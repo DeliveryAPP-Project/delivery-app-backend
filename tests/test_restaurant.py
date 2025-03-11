a71c7a31e9b4aee64988e4447a88322dae39dc14
bentoluizv <bentoluizv@gmail.com>
2025-03-10 23:14:27 -0300

    fix: update schema in restaunt test for POST endpoint

--- tests/test_restaurant.py
+++ tests/test_restaurant.py
@@ -1,7 +1,8 @@
 def test_list_restaurant_return_200(app_testing):
     restaurant = app_testing.test_client()
-    response = restaurant.get('http://127.0.0.1:5000/api/v1/restaurants/')
+    response = restaurant.get("http://127.0.0.1:5000/api/v1/restaurants/")
     assert response.status_code == 200
+
 
 def test_post_restaurant_return_200(app_testing):
     client = app_testing.test_client()
@@ -12,9 +13,10 @@ def test_post_restaurant_return_200(app_testing):
         "classification": 4.5,
         "location": "Cidade do Teste",
         "url_image_logo": "url_logo_teste",
-        "url_image_banner": "url_banner_teste"
+        "url_image_banner": "url_banner_teste",
+        "telephone": "12345678901",
+        "has_plastic": False,
     }
 
-    response = client.post('/api/v1/restaurants/', json=restaurant_data)
-   
+    response = client.post("/api/v1/restaurants/", json=restaurant_data)
     assert response.status_code == 201
