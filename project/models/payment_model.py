405dd5ab072353fd2e4ac8004e066619fa2054f3
bentoluizv <bentoluizv@gmail.com>
2025-03-16 16:57:38 -0300

    fix: rm func.now due to sqlite error

--- project/models/payment_model.py
+++ project/models/payment_model.py
@@ -1,11 +1,11 @@
-from sqlalchemy import func
+from datetime import datetime
 
 from ..ext.database import db
 
 
 class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    created_at = db.Column(db.DateTime, default=func.now)
+    created_at = db.Column(db.DateTime, default=datetime.now)
     total_value = db.Column(db.Float)
     type = db.Column(db.Enum("Dinheiro", "Pix", name="payment_type"), nullable=False)
     status = db.Column(
