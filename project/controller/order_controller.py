e0be2fd8a0155fedec1fad2b6510ba071c9e802e
bentoluizv <bentoluizv@gmail.com>
2025-03-16 15:12:44 -0300

    fix: remove payment related code

--- project/controller/order_controller.py
+++ project/controller/order_controller.py
@@ -1,64 +1,64 @@
-from http.client import HTTPException
 import json
+from http.client import HTTPException
 
-from flask import request
+from flask import abort, request
 from flask_restx import Resource
-from project.ext.serializer import OrderSchema
-from project.service.order_service import (delete_one_order, get_all_orders,
-                                           get_one_order, post_order,
-                                           update_order)
-from project.utils.redis_utils import delete_redis_value, get_redis_value, set_redis_value
-from project.doc_model.doc_models import order_model, api
 
+from project.doc_model.doc_models import api, order_model
+from project.ext.serializer import OrderSchema
+from project.service.order_service import (
+    create_order,
+    delete_order,
+    get_all_orders,
+    get_order,
+    update_order,
+)
+from project.utils.redis_utils import (
+    delete_redis_value,
+    get_redis_value,
+    set_redis_value,
+)
 
 order_schema_list = OrderSchema(many=True)
 order_schema = OrderSchema(many=False)
 
 
 class OrderResource(Resource):
-
     def get(self):
         key_redis = "orders"
         orders = get_redis_value(key_redis)
         if orders:
-            return orders
+            return orders, 200
         orders = get_all_orders()
         orders = order_schema_list.dump(orders)
         set_redis_value(key_redis, json.dumps(orders))
         return orders, 200
 
-
     @api.expect(order_model)
     def post(self):
         try:
             order_data = request.json
-            if order_data['payment'] == "Pix" or order_data['payment'] == "Dinheiro":
-                response = post_order(order_data)
-                if response and order_data['payment'] == 'Pix':
-                    return {"message": "Pedido cadastrado com sucesso!", "pix_copia_e_cola": response['pixCopiaECola'], "base64": response['pixBase64']}, 201
-                elif order_data['payment'] == 'Dinheiro':
-                    return {"message": "Pedido cadastrado com sucesso!"}
-            else:
-                return {"message": "Meio de pagamento inválido. (Opções: Pix e Dinheiro)"}
+            create_order(order_data)  # type: ignore
             delete_redis_value("orders")
-        
+
         except Exception as e:
             return {"error": str(e)}, 400
 
 
 class OrderResourceID(Resource):
-
     def get(self, id: int):
-        if order := get_one_order(id):
+        if order := get_order(id):
             return order_schema.dump(order), 200  # type: ignore
         else:
             return {"error": f"Ordem com ID {id} não encontrado."}, 404
-        
 
     @api.expect(order_model)
     def patch(self, id: int):
         try:
             order_data = request.json
+            if order_data is None:
+                return abort(400, "error Dados do pedido não fornecidos.")
+
             result = update_order(id, order_data)
             delete_redis_value("clients")
             return {"message": result["message"]}, 200
@@ -69,10 +69,9 @@ class OrderResourceID(Resource):
         except Exception as e:
             return {"error": str(e)}, 500
 
-
     def delete(self, id: int):
         try:
-            result = delete_one_order(id)
+            result = delete_order(id)
 
             if "error" in result:
                 return {"error": result["error"]}, 404
@@ -81,4 +80,3 @@ class OrderResourceID(Resource):
 
         except Exception as e:
             return {"error": str(e)}, 500
-        
