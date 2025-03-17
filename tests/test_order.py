77a73bafc4f5e5c189543905cab3d4f427c5f1e3
bentoluizv <bentoluizv@gmail.com>
2025-03-16 21:40:40 -0300

    test: enhance order tests for retrieval, update, and deletion scenarios

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,20 +1,54 @@
-def test_list_order_return_200(app_testing):
-    order = app_testing.test_client()
-    response = order.get("http://127.0.0.1:5000/api/v1/orders/")
+def test_get_order_by_id_return_200(app_testing):
+    client = app_testing.test_client()
+    response = client.get("/api/v1/orders/1")
     assert response.status_code == 200
+    assert response.json["id"] == 1
 
 
-def test_post_order_return_200(app_testing):
+def test_get_order_by_id_return_404(app_testing):
+    client = app_testing.test_client()
+    response = client.get("/api/v1/orders/999")
+    assert response.status_code == 404
+    assert response.json["error"] == "Ordem com ID 999 não encontrado."
+
+
+def test_update_order_return_200(app_testing):
     client = app_testing.test_client()
 
-    order_data = {
-        "client_id": 1,
-        "restaurant_id": 1,
+    updated_order_data = {
         "products": [1],
-        "total_value": 100.0,
-        "status": "pre_order",
+        "total_value": 150.0,
+        "status": "confirmed",
     }
 
-    response = client.post("/api/v1/orders/", json=order_data)
+    response = client.patch("/api/v1/orders/1", json=updated_order_data)
+    assert response.status_code == 200
+    assert response.json["message"] == "Ordem com ID 1 atualizado com sucesso!"
 
-    assert response.status_code == 201
+
+def test_update_order_return_404(app_testing):
+    client = app_testing.test_client()
+
+    updated_order_data = {
+        "products": [1],
+        "total_value": 150.0,
+        "status": "confirmed",
+    }
+
+    response = client.patch("/api/v1/orders/999", json=updated_order_data)
+    assert response.status_code == 404
+    assert response.json["message"] == "NotFoundError: Ordem com ID 999 não encontrado"
+
+
+def test_delete_order_return_200(app_testing):
+    client = app_testing.test_client()
+    response = client.delete("/api/v1/orders/1")
+    assert response.status_code == 200
+    assert response.json["message"] == "Ordem com ID 1 deletado com sucesso."
+
+
+def test_delete_order_return_404(app_testing):
+    client = app_testing.test_client()
+    response = client.delete("/api/v1/orders/999")
+    assert response.status_code == 404
+    assert response.json["message"] == "NotFoundError: Ordem com ID 999 não encontrado"
